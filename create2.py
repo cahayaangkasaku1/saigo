8ebf52fa70943cb2085f13ec13a2dd97bc0505dd
cahayaangkasaku1 <147915222+cahayaangkasaku1@users.noreply.github.com>
2024-10-24 01:01:02 +0700

    Update create2.py

--- create2.py
+++ create2.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
     # Buat 10 email acak
     for _ in range(10):
         random_name = generate_random_name()
-        new_email = f"mr.platra13+{random_name}@butyusa.com"
+        new_email = f"dedenkurnia28.dk+{random_name}@gmail.com"
         generated_emails.append(new_email)
         
         # Simpan email baru ke dalam file test.txt
